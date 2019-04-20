1a27d1c3ca81bc0be2b12bbc8b21e725e05672d6
Ben Huggins <benshuggins@gmail.com>
2019-04-20 10:41:57 -0600

    first commit to master

---
+++ FBSocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBSocial
+//
+//  Created by Ben Huggins on 4/20/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
